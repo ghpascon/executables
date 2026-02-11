dbc269da48ebd8108b3cb833d0eb4c83538992fe
Gabriel Henrique Pascon <152620285+ghpascon@users.noreply.github.com>
2026-02-11 15:02:20 -0300

    Delete windows directory

--- windows/main/_internal/app/services/__init__.py
+++
@@ -1,4 +0,0 @@
-from .rfid._main import RfidManager
-from app.core import DEVICES_PATH, EXAMPLE_PATH
-
-rfid_manager = RfidManager(devices_path=DEVICES_PATH, example_path=EXAMPLE_PATH)
--- windows/main/_internal/app/services/settings_service/__init__.py
+++
@@ -1,3 +0,0 @@
-from ._main import SettingsService
-
-settings_service = SettingsService()
